012e7b1094dd6f4b73393658231465cc3eee9384
xzycomputer <anandajaisuk@gmail.com>
2023-05-19 14:05:42 +0700

    edit connect

--- connect.js
+++ connect.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sql = require('mssql');
 const app = express();
+app.use(express.json());
 
 const config = {
   server: '203.154.222.73',
@@ -27,6 +28,28 @@ app.get('/data', async (req, res) => {
   const query = 'SELECT * FROM m_member';
   const data = await executeQuery(query);
   res.json(data);
+});
+
+app.post('/data', async (req, res) => {
+  const { column1, column2 } = req.body; // Assuming the request body contains column1 and column2 values
+  const query = `INSERT INTO m_member (column1, column2) VALUES ('${column1}', '${column2}')`;
+  await executeQuery(query);
+  res.send('Data added successfully');
+});
+
+app.put('/data/:id', async (req, res) => {
+  const { id } = req.params; // Assuming the ID is passed as a URL parameter
+  const { column1, column2 } = req.body; // Assuming the request body contains column1 and column2 values
+  const query = `UPDATE m_member SET column1 = '${column1}', column2 = '${column2}' WHERE id = ${id}`;
+  await executeQuery(query);
+  res.send('Data updated successfully');
+});
+
+app.delete('/data/:id', async (req, res) => {
+  const { id } = req.params; // Assuming the ID is passed as a URL parameter
+  const query = `DELETE FROM m_member WHERE id = ${id}`;
+  await executeQuery(query);
+  res.send('Data deleted successfully');
 });
 
 app.listen(3000, () => {
